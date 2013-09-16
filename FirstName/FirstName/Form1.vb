4037cec86389d872d7ff1844f44d4c06333be342
idsc3101 <idsc3101@gmail.com>
2013-09-16 15:25:07 -0500

    Solved FirstName Problem

--- FirstName/FirstName/Form1.vb
+++ FirstName/FirstName/Form1.vb
@@ -4,5 +4,20 @@ Public Class Form1
 
     Private Sub btnFirstName_Click(sender As System.Object, e As System.EventArgs) Handles btnFirstName.Click
 
+        Dim fullname As String
+
+        fullname = txtFullName.Text
+
+        Dim fullnameTrimmed As String
+        fullnameTrimmed = fullname.Trim()
+
+        Dim idxSpace As Integer
+        idxSpace = fullnameTrimmed.IndexOf(" ")
+
+        Dim firstName As String
+        firstName = fullnameTrimmed.Substring(0, idxSpace)
+
+        txtFirstName.Text = firstName
+
     End Sub
 End Class
